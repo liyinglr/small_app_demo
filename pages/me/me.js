6cfa3b63efb967a5e44537b78ed0a496906341e9
liyinglr <liyinglr@gmail.com>
2017-07-14 00:06:27 +0800

    增加weui style文件

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,4 +1,6 @@
 // me.js
+//获取应用实例
+var app = getApp()
 Page({
 
   /**
@@ -12,7 +14,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function (userInfo) {
+      //更新数据
+      console.log(userInfo)
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   },
 
   /**
