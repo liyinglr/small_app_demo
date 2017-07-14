2499c7987e5703531e79be876956f013fa4a48e5
liyinglr <liyinglr@gmail.com>
2017-07-14 17:54:45 +0800

    增加edit/rpofile页面

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 //app.js
 App({
   onLaunch: function() {
+    wx.login({
+      success:function(res){
+        console.log(res)
+        // var js_code = res.code
+        // wx.request({
+        //   url: 'https://api.weixin.qq.com/sns/jscode2session',
+        //   data:{
+        //     "appid":"wx0c9f0fefada9be85",
+        //     "secret":"",
+        //     "js_code":js_code,
+        //     "grant_type":"authorization_code"
+        //   },
+        //   success: function (e) {
+        //     console.log(e)
+        //   }
+        // })
+      }
+    })
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
